912789b7fdfb3eb316464e7a65a22670a8a8a08b
chaos-warzh <haidedeyu@163.com>
2023-09-17 17:28:11 +0800

    +Sub

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -107,15 +107,58 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 #endif
 }
 
+void set_CF_sub(result, dest, data_size) {
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.CF = result > dest;
+}
+
+void set_OF_sub(uint32_t result, uint32_t src, uint32_t dest, size_t data_size) {
+	switch(data_size) {
+		case 8: 
+			result = sign_ext(result & 0xFF, 8); 
+			src = sign_ext(src & 0xFF, 8); 
+			dest = sign_ext(dest & 0xFF, 8); 
+			break;
+		case 16: 
+			result = sign_ext(result & 0xFFFF, 16); 
+			src = sign_ext(src & 0xFFFF, 16); 
+			dest = sign_ext(dest & 0xFFFF, 16); 
+			break;
+		default: break;// do nothing
+	}
+	if (sign(src) == sign(dest)) {
+		cpu.eflags.OF = 0;// won't overflow
+	} else {
+		if (sign(dest) != sign(result)) {
+			cpu.eflags.OF = 1;
+		} else {
+			cpu.eflags.OF = 0;
+		}
+	}
+}
+
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	// printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	// fflush(stdout);
+	// assert(0);
+
+	// dest - sign_extend(src)
+	uint32_t res = dest - src;
+
+	// alu_add(-src, dest, data_size);
+	set_CF_sub(res, dest, data_size);   // 设置标志位
+	set_PF(res);
+	
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_OF_sub(res, src, dest, data_size);
+	
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
